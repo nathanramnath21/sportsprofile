c8c107b4aa0f75b7d24e51696574f7cb02dbe585
nathanramnath21 <71851194+nathanramnath21@users.noreply.github.com>
2021-04-28 15:59:29 -0400

    Add files via upload

---
+++ classfolder/position.py
@@ -0,0 +1,11 @@
+class SportProfile(object):
+    def __init__(self, name, age, height, team, position):
+        self.name=name
+        self.age=age
+        self.height=height
+        self.team=team
+        self.position=position
+
+    def display(self, name, age, height, team, position):
+     return self.name
+
